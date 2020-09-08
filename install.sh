3ab8dee3f97fbbaccb98372fd9c205510ef3435c
eBulldog <62420327+eBulldog@users.noreply.github.com>
2020-09-08 19:06:45 +0100

    Update install.sh
    Ensure script can be run from anywhere and recognizes its location.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cp -R module/exim4 /usr/share/filebeat/module/
-cp modules.d/exim4.yml.disabled /etc/filebeat/modules.d/
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+cp -R $SCRIPTPATH/module/exim4 /usr/share/filebeat/module/
+cp $SCRIPTPATH/modules.d/exim4.yml.disabled /etc/filebeat/modules.d/
 filebeat modules enable exim4
